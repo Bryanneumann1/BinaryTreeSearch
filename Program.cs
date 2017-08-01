9dcc66fbe7f595595c65750c5dc73135fd163040
Bryan Neumann <Bryan Neumann>
2017-08-01 16:09:59 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            BinarySearchTree binaryTree = new BinarySearchTree();
+
+            binaryTree.Add(9);
+            binaryTree.Add(6);
+            binaryTree.Add(2);
+            binaryTree.Add(4);
+            binaryTree.Add(5);
+            binaryTree.Add(3);
+
+            Console.WriteLine("Tree is:");
+            binaryTree.Display();
+            Console.WriteLine("\nCount = " + binaryTree.Count);
+            Console.WriteLine("\nAdd 99");
+            binaryTree.Add(99);
+            Console.WriteLine("\nCount = " + binaryTree.Count);
+            Console.WriteLine("Search for 9 " + binaryTree.Search(9));
+            Console.WriteLine("Search for 3 " + binaryTree.Search(3));
+            Console.WriteLine("Search for 7 " + binaryTree.Search(7));
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ node.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Node
+    {
+        public Node leftChild;
+        public Node rightChild;
+        public int number;
+
+        public Node(int number)
+        {
+            this.number = number;
+            rightChild = null;
+            leftChild = null;
+        }
+        public Node InsertData(Node node, int data)
+        {
+            if (node == null)
+            {
+                node = new Node(data);
+            }
+            else if (node.number > data)
+            {
+                node.leftChild = InsertData(node.leftChild, data);
+            }
+            else if (node.number < data)
+            {
+                node.rightChild = InsertData(node.rightChild, data);
+            }
+            return node;
+        }
+
+        public void Display(Node n)
+        {
+            if (n == null)
+                return;
+
+            Display(n.leftChild);
+            Console.Write(n.number.ToString() + " ");
+            Display(n.rightChild);
+        }
+
+
+    }
+}
